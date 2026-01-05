4ed29b7e98d61375c7710ae678247ed676c7f0f7
Trptguy09 <nervouspuddin@gmail.com>
2026-01-05 14:29:33 -0500

    added endpoints for healthz, reset, metrics and handlers for them

--- main.go
+++ main.go
@@ -1,18 +1,64 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) healthz(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) metrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, `<html>
+  <body>
+    <h1>Welcome, Chirpy Admin</h1>
+    <p>Chirpy has been visited %d times!</p>
+  </body>
+</html>`, cfg.fileserverHits.Load())
+}
+
+func (cfg *apiConfig) reset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "Hits: %d", cfg.fileserverHits.Load())
+
+}
+
 func main() {
+	apiCfg := &apiConfig{}
 	serverMux := http.NewServeMux()
+	handler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	serverMux.Handle("/app/", apiCfg.middlewareMetricsInc(handler))
+	serverMux.Handle("/app", apiCfg.middlewareMetricsInc(handler))
+	serverMux.HandleFunc("GET /api/healthz", apiCfg.healthz)
+	serverMux.HandleFunc("GET /admin/metrics", apiCfg.metrics)
+	serverMux.HandleFunc("POST /admin/reset", apiCfg.reset)
 
 	srv := http.Server{
 		Addr:    ":8080",
 		Handler: serverMux,
 	}
-	serverMux.Handle("/", http.FileServer(http.Dir(".")))
 
-	srv.ListenAndServe()
+	if err := srv.ListenAndServe(); err != nil {
+		panic(err)
+	}
 
 }
